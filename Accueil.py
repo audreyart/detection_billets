73a14014825b0e38687543e384d64b89ca0a2d88
audreyart <93116282+audreyart@users.noreply.github.com>
2022-12-14 22:32:42 +0400

    Delete Accueil.py

--- Accueil.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Accueil")
-
-st.title("Bienvenue dans l'application de détection de faux billets")
-
-st.subheader("Vous êtes sur la page d'accueil")
-
-st.markdown("Ce site est destiné à vous présenter l'application de détection de faux billets mise en place lors de ma formation de Data Analyst avec Openclassrooms. J'ai découvert streamlit après la fin de ma formation. Etant destiné au déploiement des applications de machine learning notamment, j'ai donc décidé d'utiliser cet outil afin de déployer l'algorithme de détection de vrais et de faux billets.")
