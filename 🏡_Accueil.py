cd860b09f0b8c55bc6fd554de4f000c603bbbb85
audreyart <93116282+audreyart@users.noreply.github.com>
2022-12-14 22:41:09 +0400

    Update 🏡_Accueil.py

--- 🏡_Accueil.py
+++ 🏡_Accueil.py
@@ -6,4 +6,4 @@ st.title("Bienvenue dans l'application de détection de faux billets")
 
 st.subheader("Vous êtes sur la page d'accueil")
 
-st.markdown("Ce site est destiné à vous présenter l'application de détection de faux billets mise en place lors de ma formation de Data Analyst avec Openclassrooms. J'ai découvert streamlit après la fin de ma formation. Etant destiné au déploiement des applications de machine learning notamment, j'ai donc décidé d'utiliser cet outil afin de déployer l'algorithme de détection de vrais et de faux billets.")
+st.markdown("Ce site est destiné à la présentation de l'application de détection de faux billets mise en place lors de ma formation de Data Analyst avec Openclassrooms. J'ai découvert streamlit après la fin de ma formation. Etant destiné au déploiement des applications de machine learning notamment, j'ai donc décidé d'utiliser cet outil afin de déployer l'algorithme de détection de vrais et de faux billets.")
